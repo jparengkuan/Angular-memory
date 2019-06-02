30cb1685c0f913a1e32845c14d6fd595b52e19e7
Youri van de Geer <yourivandegeer123@gmail.com>
2019-06-02 22:56:47 +0200

    Basic updates to routing and logging in. For reference contact me. Tasks: package game and seperate from auth. Use auth as login or make a new module. Add both modules (login(or auth) and game) to the routing module. use link: https://www.youtube.com/watch?v=F1GUjHPpCLA&t=87s for an example to the login functionalities.

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,25 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// //import { GameComponent } from '../game/game.component'
+//
+// const routes: Routes = [
+//   {
+//     path: '',
+//     // component: DashboardComponent,
+//   },
+//   {
+//     path: '',
+//     //  component: LoginComponent,
+//   }
+// ];
+//
+// @NgModule({
+//   imports: [
+//     RouterModule.forRoot(routes)
+//   ],
+//   exports: [
+//     RouterModule
+//   ],
+//   declarations: []
+// })
+// export class AppRoutingModule { }
